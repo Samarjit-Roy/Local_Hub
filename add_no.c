dc3a1e688ead19ec3a4e19116d5f95eaf487cebc
Samarjit <samarjitk221@gmail.com>
2024-12-22 00:05:42 +0530

    Add 2 no

---
+++ add_no.c
@@ -0,0 +1,12 @@
+// Create a program to add 2 no...
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int no1,no2,sum;
+    printf("Enter two no=");
+    scanf("%d%d",&no1,&no2);
+    sum=no1+no2;
+    printf("The sum of %d & %d is= %d",no1,no2,sum);
+    return 0;
+}
